d87a661e537d5d51d7ee5588eff98763df98cbfb
Maxence <maxence.fleurisson-coquibus@epfedu.fr>
2025-12-18 18:12:22 +0100

    Projet Onitama : Classe Joueur terminée

--- Projet_Onitama/src/Joueur.java
+++ Projet_Onitama/src/Joueur.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +11,33 @@
  * @author maxen
  */
 public class Joueur {
+    private ArrayList<Piece> pieces;
+    private CarteDeplacement carte1;
+    private CarteDeplacement carte2;
+
+    public Joueur(ArrayList<Piece> pieces, CarteDeplacement carte1, CarteDeplacement carte2) {
+        this.pieces = pieces;
+        this.carte1 = carte1;
+        this.carte2 = carte2;
+    }
+
+    public ArrayList<Piece> getPieces() {
+        return pieces;
+    }
+
+    public CarteDeplacement getCarte1() {
+        return carte1;
+    }
+
+    public CarteDeplacement getCarte2() {
+        return carte2;
+    }
     
+    public void remplacerCarte(CarteDeplacement ancienne, CarteDeplacement nouvelle){
+        if (carte1 == ancienne) {
+            carte1 = nouvelle;
+        } else if (carte2 == ancienne){
+            carte2 = nouvelle;
+        }
+    }
 }
