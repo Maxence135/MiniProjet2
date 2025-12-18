b0123eab90f79d3d0ecc13f156f813b1f3799eb8
Maxence <maxence.fleurisson-coquibus@epfedu.fr>
2025-12-18 17:58:48 +0100

    Projet Onitama : Classe ListeCartes et Piece terminé

--- Projet_Onitama/src/Piece.java
+++ Projet_Onitama/src/Piece.java
@@ -8,5 +8,28 @@
  * @author maxen
  */
 public class Piece {
+    public enum Type {Roi,Pion};
+    public enum Couleur {Rouge,Bleu};
     
+    private Type type;
+    private Couleur couleur;
+    private int x,y;
+
+    public Piece(Type type, Couleur couleur, int x, int y) {
+        this.type = type;
+        this.couleur = couleur;
+        this.x = x;
+        this.y = y;
+    }
+    
+    public int getX() {return x;}
+    public int getY() {return y;}
+    
+    public void deplacer(int nx, int ny){
+        this.x = nx;
+        this.y = ny;
+    }
+    
+    public Type getType() {return type;}
+    public Couleur getCouleur(){return couleur;}
 }
